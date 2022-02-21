9df439f1b50b21d46eb058678e3993f3e88ee0ee
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-20 21:58:44 -0600

    added Navbar.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import React, {useEffect, useState} from 'react'
 import Recipe from './components/Recipe';
+import Navbar from './components/Navbar';
 
 
 
@@ -15,13 +16,19 @@ function App(){
   
   const [recipes, setRecipes] = useState([])
   const [searchBar, setSearchBar] = useState("")
+  const [query, setQuery] = useState('chicken')
 
 function handleSearchBar(event) {
   setSearchBar(event.target.value)
 }  
+
+function handleQuery(event) {
+  event.preventDefault()
+  setQuery(searchBar)
+}
   
   useEffect(() => {
-    fetch(`https://api.edamam.com/search?q=chicken&app_id=${AppId}&app_key=${AppKey}`)
+    fetch(`https://api.edamam.com/search?q=${query}&app_id=${AppId}&app_key=${AppKey}`)
     .then((response) => response.json())
     .then((data) => setRecipes(data.hits))
   }, [])
@@ -31,18 +38,24 @@ console.log(searchBar)
 
   return(
     <div className='App' >
-      <h1>
-        Hello react
+      
+      <h1 className='header'>
+        Insta {<img className='picture' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVTGuFydYGbqBCW8lavBmMdrMFyX4D-Hv9iGjxKBbbKojgWi7wGunC7-JzaPRKW3VYg_Y&usqp=CAU' alt='fork-pic' />} cook
       </h1>
-      <form>
-        <input type="text" placeholder='Search for any recipe!' value={searchBar} onChange={handleSearchBar}></input>
-        <button type='submit'>Search</button>
+      <div className='navbar'>
+        <Navbar  />
+      </div>
+      <form className='form-submit' onSubmit={handleQuery} >
+        <input className='search-bar' type="text" placeholder='Search for any recipe!' value={searchBar} onChange={handleSearchBar}></input>
+        <button className='search-button' type='submit'>Search</button>
       </form>
       <div className='recipes' >
       {recipes.map(recipe => (
         <Recipe title={recipe.recipe.label}
         calories={recipe.recipe.calories} 
         image={recipe.recipe.image}
+        
+        
         
         />
       ))}
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Recipe({title,calories,image}){
+function Recipe({title,calories,image,ingredients}){
     return(
         <div>
             <h1>{title}</h1>
             <p>Calories: {calories}</p>
+            <p>{ingredients}</p>
             <img src={image} alt="" />
 
         </div>
